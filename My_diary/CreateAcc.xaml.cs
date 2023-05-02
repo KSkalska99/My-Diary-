7ceb9783dc5c259fc1b6f029ecadd4853c812cc5
Katarzyna Skalska <katarzyna.skalska@edu.uni.lodz.pl>
2023-05-02 19:27:54 +0200

    email, pswd correction

--- My_diary/CreateAcc.xaml.cs
+++ My_diary/CreateAcc.xaml.cs
@@ -36,26 +36,80 @@ namespace My_diary
         private void cr_acc_clear_btn_Click(object sender, RoutedEventArgs e)
         {
             AccCr_log_txtBox.Clear();
-            AccCr_log_txtBox.Clear();
-            //AccCr_email_txtBox.Clear();
+            AccCr_pswd_txtBox.Clear();
+            AccCr_email_txtBox.Clear();
         }
-        public static string CorrectEmail()
+        public void CorrectEmail()
         {
-            string strEMail = "youremail@host.com";
-            Regex regx = new Regex(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$");
-            Match match = regx.Match(strEMail);
-            if (match.Success)
+            var email = AccCr_email_txtBox.Text;
+
+            if (string.IsNullOrWhiteSpace(email))
             {
-                MessageBox.Show("Your login is correct");
+                MessageBox.Show("Your email is not correct");
+            }
+            else if (!email.Contains("@"))
+            {
+                MessageBox.Show("Your email is not correct");
+            }
+            else if (email.Count(count => count == '.') < 1)
+            {
+                MessageBox.Show("Your email is not correct");
             }
             else
             {
-                MessageBox.Show("Your login is not correct");
+                MessageBox.Show("Your email is correct");
             }
-            return strEMail;
 
-                
+            //return true;
+
         }
 
+        public bool IsPasswordValid(string password)
+        {
+            bool hasUppercaseLetter = false;
+            bool hasDigit = false;
+            bool hasSpecialCharacter = false;
+            bool hasEightChars = password.Length >= 8;
+
+            foreach (char znak in password)
+            {
+                if (char.IsUpper(znak))
+                {
+                    hasUppercaseLetter = true;
+                }
+                else if (char.IsDigit(znak))
+                {
+                    hasDigit = true;
+                }
+                else if (char.IsSymbol(znak) || char.IsPunctuation(znak))
+                {
+                    hasSpecialCharacter = true;
+                }
+            }
+
+
+            return hasUppercaseLetter && hasDigit && hasSpecialCharacter && hasEightChars;
+        }
+        private void ok_cracc_btn_Click(object sender, RoutedEventArgs e)
+        {
+            string password = AccCr_pswd_txtBox.Text;
+            bool isPasswordValid = IsPasswordValid(password);
+
+            if (isPasswordValid)
+            {
+                MessageBox.Show("Your password is correct");
+            }
+            else
+            {
+                MessageBox.Show("Your password is not correct");
+            }
+            IsPasswordValid(isPasswordValid.ToString());
+            if (isPasswordValid != true)
+            {
+                MessageBox.Show("Your password requires min 8 letters \n with min one uppercase letter, \n " +
+                  "one digit and one special character");
+            }
+            CorrectEmail();
+        }
     }
 }
